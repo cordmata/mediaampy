f9ac8c84bccfb6265909b67afe37af131b3cb295
Matt Cordial <matt.cordial@gmail.com>
2015-04-28 18:16:30 -0700

    Remove normalize name.
    This is not something we need.

--- mediaamp/utils.py
+++ mediaamp/utils.py
@@ -2,10 +2,6 @@ from datetime import datetime
 from time import mktime
 
 
-def normalize_registry_name(name):
-    name.lower().replace(' ', '_')
-
-
 def decode_datetime(dt_in_millis):
     return datetime.utcfromtimestamp(dt_in_millis / 1000)
 
