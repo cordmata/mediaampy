618d3d34496e703e4f633f38aa11a8ba3b34a303
Matt Cordial <matt.cordial@asu.edu>
2015-06-25 15:52:51 -0700

    Use timegm instead of mktime.

--- mediaamp/__init__.py
+++ mediaamp/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 __title__ = 'mediaampy'
 
 from .http import Session
--- mediaamp/utils.py
+++ mediaamp/utils.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-from time import mktime
+from calendar import timegm
 from pytz import UTC
 
 
 def decode_datetime(dt_in_millis):
-    return datetime.fromtimestamp(dt_in_millis / 1000).replace(tzinfo=UTC)
+    return datetime.fromtimestamp(dt_in_millis / 1000, UTC)
 
 
 def encode_datetime(dt):
     if dt.tzinfo is None or dt.tzinfo.utcoffset(dt) is None:
         # if naive assume UTC as that is what the API expects
         dt = UTC.localize(dt)
-    return int(mktime(dt.astimezone(UTC).timetuple()) * 1000)
+    return timegm(dt.astimezone(UTC).timetuple()) * 1000
